618be515e909e72e170460b0844ec0fed11f97b9
Adiks <apacholak97@interia.pl>
2021-04-25 17:14:26 +0200

    Creating your own configuration properties

--- src/main/java/pl/adiks/tacocloud/controller/OrderController.java
+++ src/main/java/pl/adiks/tacocloud/controller/OrderController.java
@@ -1,14 +1,19 @@
 package pl.adiks.tacocloud.controller;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import pl.adiks.tacocloud.config.OrderProps;
 import pl.adiks.tacocloud.domain.Order;
 import pl.adiks.tacocloud.domain.User;
 import pl.adiks.tacocloud.repository.OrderRepository;
@@ -20,10 +25,12 @@ import javax.validation.Valid;
 @SessionAttributes(names = "order")
 public class OrderController {
 
+    private OrderProps orderProps;
     private final OrderRepository orderRepository;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
     }
 
     @GetMapping("/current")
@@ -43,5 +50,14 @@ public class OrderController {
         sessionStatus.setComplete();
 
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
     }
 }
--- src/main/java/pl/adiks/tacocloud/repository/OrderRepository.java
+++ src/main/java/pl/adiks/tacocloud/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package pl.adiks.tacocloud.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.adiks.tacocloud.domain.Order;
+import pl.adiks.tacocloud.domain.User;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
